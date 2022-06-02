c2ad4ab3624f8224cbb99e4e613e97fc8d2848fd
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-06-01 22:42:52 -0400

    code challenge 1

---
+++ 1-Fundamentals/cc1.py
@@ -0,0 +1,22 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 5.11
+print(type(b))
+
+c = "jeff"
+print(type(c))
+
+d = a + b
+print(d)
+
+
+
+
+
+
+
