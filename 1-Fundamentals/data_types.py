bfc715d96fb5405c26f194d47704c8508c65ce4c
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-06-01 22:04:53 -0400

    data types & variables

---
+++ 1-Fundamentals/data_types.py
@@ -0,0 +1,38 @@
+"""
+Declaring variables
+"""
+
+# x = 12.2
+# y = 7
+# z = 9.0
+# print(x)
+# print(y)
+# print(z)
+
+"""
+Primitive data types
+"""
+name = "Jeff"
+age = 44
+cash = 1000.49
+retired = False
+
+# How to know the data type of a variable
+# Invoking the function 'type(<VARIABLE_NAME>)'
+# print("Data type for the variable 'name' is:", type(name))
+# print("Data type for the variable 'age' is:", type(age))
+# print("Data type for the variable 'cash' is:", type(cash))
+# print("Data type for the variable 'retired' is:", type(retired))
+
+"""
+Composite data types
+"""
+nucamp_locations = ["Seattle", "Tacoma", "Bellevue"]
+Jeff_Info = {"name": "Jeff", "age": 44, "cash": 1000.25, "retired": True}
+my_tuple = ("apple", "banana", "cherry")
+my_set = {"cats", "dogs", "birds"}
+
+print("Data type of 'nucamp_locations' is:", type(nucamp_locations))
+print("Data type of 'Jeff_Info' is:", type(Jeff_Info))
+print("Data type of 'my_tuple' is:", type(my_tuple))
+print("Data type of 'my_set' is:", type(my_set))
