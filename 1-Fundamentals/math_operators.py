243461d24d5e230aa24a31193af9f57d8e912066
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-06-01 22:28:27 -0400

    math operators

---
+++ 1-Fundamentals/math_operators.py
@@ -0,0 +1,61 @@
+"""
+Addition
+"""
+# x = 1
+# y = 2
+# z = 3
+# xyz = x + y + z  # xyz = 1 + 2 + 3     = 6
+# x = x + 10       # x   = 1 + 10        = 11
+# y = y + 7        # y   = 2 + 7         = 9
+# z = z + 9        # z   = 3 + 9         = 12
+
+"""
+Subtraction
+"""
+# x = 10
+# y = 2
+# z = 3
+# xyz = x - y - z  # xyz   = 10 - 2 - 3    = 5
+# x = x - 10       # x     = 10 - 10       = 0
+# y = y - 7        # y     = 2 - 7         = -5
+# z = z - 9        # z     = 3 - 9         = -6
+
+"""
+Multiplication
+"""
+# x = 1
+# y = 2
+# z = 3
+# xyz = x * y * z    # xyz    = 1 * 2 * 3     = 6
+# x = x * 10         # x      = 1 * 10        = 10 
+# y = y * 7          # y      = 2 * 7         = 14
+# z = z * 9          # z      = 3 * 9         = 27
+
+"""
+Division
+"""
+# x = 20 
+# y = 5 
+# z = 4
+# xyz = x / y / z      # zyx    = 20 / 5 / 4    = 1.0 (Note: float value)
+# x = x / 10           # x      = 20 / 10       = 2.0
+# y = y / 5            # y      = 5 / 5         = 1.0
+# z = z / 3            # z      = 4 / 3         = 1.3
+
+"""
+Floor Division
+"""
+# x = 20 
+# y = 5 
+# z = 4
+# xyz = x // y // z      # zyx    = 20 // 5 // 4    = 1 (Note: integer value)
+# x = x // 10           # x      = 20 // 10       = 2
+# y = y // 5            # y      = 5 // 5         = 1
+# z = z // 3            # z      = 4 // 3         = 1
+
+""".
+Exponentiation
+"""
+x = 2 ** 0      # 1
+y = 2 ** 1      # 2
+z = 2 ** 2.0    # 4.0
