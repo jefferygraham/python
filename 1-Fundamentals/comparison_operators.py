d2de2d6f08185f851816e00143ca4eaec1dfa41d
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-06-01 22:35:03 -0400

    comparison operators

---
+++ 1-Fundamentals/comparison_operators.py
@@ -0,0 +1,11 @@
+"""
+Python comparison operators
+"""
+x = 100
+y = 200
+
+print(x != y)       # x is not equal to y
+print(x > y)       # x is greater than y
+print(x < y)       # x is less than y
+print(x >= y)       # x is greater than or equal to y
+print(x <= y)       # x is less than or equal to y
