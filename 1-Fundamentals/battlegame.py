7d68559563940f4b58ec45bbeca3175679d75227
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-06-02 22:17:35 -0400

    handle player choice

--- 1-Fundamentals/battlegame.py
+++ 1-Fundamentals/battlegame.py
@@ -13,7 +13,32 @@ human_damage = 20
 dragon_hp = 300
 dragon_damage = 50
 
-print("1) Wizard")
-print("2) Elf")
-print("3) Human")
-character = input("Choose your character: ")
+while True:
+  print("1) Wizard")
+  print("2) Elf")
+  print("3) Human")
+  character = input("Choose your character: ")
+  
+  if character == "1":
+    character = wizard
+    my_hp = wizard_hp
+    my_damage = wizard_damage
+    break
+  
+  if character == "2":
+    character = elf
+    my_hp = elf_hp
+    my_damage = elf_damage
+    break
+  
+  if character == "3":
+    character = human
+    my_hp = human_hp
+    my_damage = human_damage
+    break
+
+  print("Unknown character!")
+
+print("You have chosen the character: ", character)
+print("Health: ", my_hp)
+print("Damage: ", my_damage)
